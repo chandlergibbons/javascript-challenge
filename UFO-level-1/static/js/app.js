b60f1afd0b48ebdb6025791b6b22157a6c9a2d3d
Chandler Gibbons <chandlergibbons6@gmail.com>
2021-03-05 15:11:30 -0500

    code carries our all tasks meeting all requirments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,24 +2,45 @@
 var tableData = data;
 
 
+// D3 Selects
+var table = d3.select('table');
+var tbody = table.select('tbody');
+var button = d3.select("#filter-btn");
+var inputFeild = d3.select("#datetime");
 
-console.log(tableData);
 
-// YOUR CODE HERE!
-function makeTableHTML(tableData) {
-    var result = "";
-    for(var i=0; i<tableData.length; i++) {
-        result += "<tr>";
-        for(var j=0; j<tableData[i].length; j++){
-            result += "<td>"+tableData[i][j]+"</td>";
-            console.log(tableData[i][j]);
-        }
-        result += "</tr>";
-    }
-    result += "</table>";
+// handles data filter logic and returns table filtered
+button.on('click', function() {
+  
+ // clears table before new reult populates  
+  tbody.html('');
     
-    return result;
-}
+//  grabs value from filter search 
+    var selected = (inputFeild.node().value);
+  
+// filters data on search
+    const res = tableData.filter(({
+    datetime
+    }) => selected.includes(datetime));
 
+// returns filtered data     
+    ret = res;
+    res.forEach(function(oneDatapoint) {
+        var tr = tbody.append('tr');
+        
+        Object.values(oneDatapoint).forEach(function(values) {
+                tr.append('td').text(values);
+        })
+    
+});
+});
 
-
+// renders table on page load
+tableData.forEach(function (oneDatapoint) {
+        var tr = tbody.append('tr');
+    
+        Object.values(oneDatapoint).forEach(function (values) {
+            tr.append('td').text(values);
+        })
+    
+    });
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,107 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+// D3 selects
+var table = d3.select('table');
+var tbody = table.select('tbody');
+var button = d3.select("#filter-btn");
+var inputFeild = d3.select("#datetime");
+
+var city_sect = d3.select("#city");
+var state_sect = d3.select("#state");
+var country_sect = d3.select("#country");
+var shape_sect = d3.select("#shape");
+
+
+// renders data on load
+tableData.forEach(function (oneDatapoint) {
+    var tr = tbody.append('tr');
+
+    Object.values(oneDatapoint).forEach(function (values) {
+        tr.append('td').text(values);
+    })
+
+});
+
+
+// grabing new values for drop downs 
+var citydrop = tableData.map(item => item.city)
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+var statedrop = tableData.map(item => item.state)
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+var countrydrop = tableData.map(item => item.country)
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+var shapedrop = tableData.map(item => item.shape)
+    .filter((value, index, self) => self.indexOf(value) === index);
+
+
+// drop down tables creation
+citydrop.forEach(function (values) {
+    city_sect.append('option').text(values);
+});
+
+statedrop.forEach(function (values) {
+    state_sect.append('option').text(values);
+});
+
+countrydrop.forEach(function (values) {
+    country_sect.append('option').text(values);
+});
+
+shapedrop.forEach(function (values) {
+    shape_sect.append('option').text(values);
+});
+
+
+// filtration
+button.on('click', function () {
+
+    // clears old data before droping in new filtered search
+    tbody.html('');
+
+    // grabs imputs from search bar
+    var dateSelected = (inputFeild.node().value);
+    var citySelected = (city_sect.node().value);
+    var stateSelected = (state_sect.node().value);
+    var countrySelected = (country_sect.node().value);
+    var shapeSelected = (shape_sect.node().value);
+
+    // handles filter logic 
+
+    var filteredData = tableData;
+    if (dateSelected) {
+        filteredData = filteredData.filter(({ datetime }) => dateSelected.includes(datetime));
+    }
+
+    if (citySelected !== "All") {
+        console.log(citySelected, typeof (citySelected));
+        filteredData = filteredData.filter(({ city }) => citySelected.includes(city));
+    };
+
+    if (stateSelected !== "All") {
+        filteredData = filteredData.filter(({ state }) => stateSelected.includes(state));
+    };
+
+    if (countrySelected !== "All") {
+        filteredData = filteredData.filter(({ country }) => countrySelected.includes(country));
+    };
+
+    if (shapeSelected !== "All") {
+        filteredData = filteredData.filter(({ shape }) => shapeSelected.includes(shape));
+    };
+
+    // displays filter result
+
+    filteredData.forEach(function (oneDatapoint) {
+        var tr = tbody.append('tr');
+
+        Object.values(oneDatapoint).forEach(function (values) {
+            tr.append('td').text(values);
+        })
+
+    });
+});
